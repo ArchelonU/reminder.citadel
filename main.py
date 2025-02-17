5f6a4db48de6d0825b63f4c42b43c06d101dc5b2
archelon <archelon@petryaev.ru>
2025-02-18 00:59:19 +0300

    Expand readme, rename main file

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ access_token=os.environ.get("VK_BOT_TOKEN")
 
 bot_session = vk_api.VkApi(token=access_token)
 bot_api = bot_session.get_api()
-peer_id = 2000000001 # <--- СЮДА_ВСТАВИТЬ_ID_БЕСЕДЫ
+#peer_id = 2000000001 # <--- СЮДА_ВСТАВИТЬ_ID_БЕСЕДЫ
 
 #peer_id = bot_api.messages.searchConversations(q='Тест Ботов', count=1)['items'][0]['peer']['local_id']
 
@@ -21,8 +21,11 @@ def find_duty_section():
     send_duty_section(duty_number)
 
 def send_duty_section(duty_number):
-    text_message = "Напоминаю, что сегодня дежурит секция "+sections[duty_number]
-    bot_session.method("messages.send", {"peer_id":sections_chats[duty_number], "message":text_message,"random_id":0})
+    #text_message = "Напоминаю, что сегодня дежурит секция "+sections[duty_number]
+    #bot_session.method("messages.send", {"peer_id":sections_chats[duty_number], "message":text_message,"random_id":0})
+    
+    peer_id = bot_api.messages.searchConversations(q='peer=Тест Ботов', count=1)#['items'][0]['peer']['local_id']
+    print(peer_id)
 
 def main():
     #schedule.every().monday.at('09:00').do(find_duty_section)
